8b89b007c78efa784c1ab4c8b771f9cf7a63ae36
Pheraone <brechje@me.com>
2020-10-01 01:14:07 +0200

    first assignment
    Start assignments and testing if git's good

---
+++ Opdracht1/rooster.h
@@ -0,0 +1,50 @@
+#pragma once
+#include <iostream>
+
+
+class Rooster 
+{
+	private:
+		
+	public:	
+		void geefRoosterWeer() {
+			//laat rooster zien
+		std::cout << "rooster wordt weergegeven";
+
+		}
+};
+
+class Leerling {
+	private:
+		std::string naam;
+		int leeftijd;
+		
+	public:
+		void zitInKlas(/*parameter welke klas?*/) {
+			//return leerling in lijst? van de goede klas?
+			//
+		}
+
+};
+
+class Klas {
+	private:
+		int aantalLeerlingen;
+		//array van leerlingen? met max aantalLeerlingen.
+		int jaar;
+		std::string abcd;
+};
+
+class Docent {
+	private:
+		std::string naam;
+		int leeftijd;
+		
+};
+
+class Vak {
+	private:
+		std::string type;
+
+};
+
